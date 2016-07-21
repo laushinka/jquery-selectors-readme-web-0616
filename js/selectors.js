2bff78480db4d31b1445f10250bfb3fbb8edc6e3
laushinka <laurie.malau@gmail.com>
2016-07-21 16:40:25 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
-// declare your functions here...
+function paragraphSelector(){
+  let arr = $("p");
+  return arr;
+}
+
+function lastImageSelector() {
+  return $("img:last");
+}
+
+function ninjaBabySelector() {
+  return $("#baby-ninja");
+}
+
+function divSelector() {
+  return $(".pics");
+}
+
+function firstListItem() {
+  return $("#pic-list:first-child li:first-child");
+}
